5409ad951a0359ae89a92762f4dac3bc503d4b34
Anthony Patton <anthonyalanpatton@gmail.com>
2020-11-30 00:51:46 -0800

    Added a few changes

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -2,7 +2,14 @@ import javax.swing.JFrame;
 
 public class GameFrame extends JFrame {
 
-	GamePanel() {
-		
+	GameFrame() {
+
+		this.add(new GamePanel());
+		this.setTitle("Snake");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
 	}
 }
